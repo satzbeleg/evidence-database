fc4a80a48ec2c48b6f1c33d0d7e053c9a878bad1
UH <554c46@gmail.com>
2021-01-09 12:19:52 +0100

    file renamed

---
+++ psql-setup/023-evidence-evaluated.sql
@@ -0,0 +1,11 @@
+
+
+-- keep it generic
+-- internal evaluated_id (not the same as set_id in WebApp/RestAPI)
+-- username text
+-- ui_name text
+-- set_id  UUID4  (created by WebApp/RestAPI)
+-- event_history JSONB
+-- state_sentid  JSONB
+-- checkout_data JSONB
+
