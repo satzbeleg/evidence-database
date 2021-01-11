8efd7cd65e789f36f8e12a1ae57cf763aae97215
UH <554c46@gmail.com>
2021-01-11 10:00:14 +0100

    sql query examples for evidence.evaluated_bestworst table

--- examples/evidence.sql
+++ examples/evidence.sql
@@ -16,3 +16,51 @@
 -- - the text data is combined lateron inside the API(!)
 -- 
 
+
+-- 
+-- How to save evaluated example sets for BWS-UIs
+-- 
+INSERT INTO evidence.evaluated_bestworst(
+    username, ui_name, set_id, lemmata, event_history, state_sentid_map) 
+VALUES
+    (
+        'testuser456'::text, 
+        'bestworst4'::text, 
+        uuid_generate_v4()::uuid, 
+        '{"lemma1", "lemma2"}'::text[], 
+        '[{"data": "goes here"}]'::jsonb, 
+        '{"0": "idA", "1": "idB"}'::jsonb
+    ),
+    (
+        'testuser456'::text, 
+        'bestworst5'::text, 
+        uuid_generate_v4()::uuid, 
+        '{"hello", "lemma2"}'::text[], 
+        '[{"event": "is stored"}]'::jsonb, 
+        '{"0": "idB", "1": "idC"}'::jsonb
+    ),
+    (
+        'testuser789'::text, 
+        'bestworst4'::text, 
+        uuid_generate_v4()::uuid, 
+        '{"lemma1", "world"}'::text[], 
+        '[{"data": "makes sense"}]'::jsonb, 
+        '{"0": "idA", "1": "idC"}'::jsonb
+    )
+ON CONFLICT DO NOTHING 
+RETURNING set_id
+;
+
+-- 
+-- WHERE clause on an Array column
+-- 
+SELECT * FROM evidence.evaluated_bestworst WHERE  'lemma1' = ANY(lemmata);
+
+-- 
+-- Duplicate rows for each Lemma
+-- 
+SELECT set_id, event_history, unnest(lemmata) 
+  FROM evidence.evaluated_bestworst 
+ WHERE  'lemma1' = ANY(lemmata)
+;
+
