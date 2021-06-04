76c40ef4f97cf0ffed9bd53c7b3b6f3fc2d53cac
UH <554c46@gmail.com>
2021-06-04 21:22:11 +0200

    remove demo data insert from setup

---
+++ demo-data/029-evidence.sql
@@ -0,0 +1,121 @@
+
+-- 
+-- evidence.sentences_cache
+-- 
+INSERT INTO evidence.sentences_cache(sentence_id, sentence_text, annotation) VALUES
+(
+    '21152788-5dcd-4e4b-80e2-e476b242afbc', 'Impeachment ist derzeit ein Begriff mit besonderer Schlagkraft.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [0, 11]}], "compound": []}'::jsonb
+)
+,(
+    'b4f02652-dc85-42a3-a51f-c19890ade812', 'Nach den jüngsten Ausschreitungen am und im Washingtoner Kapitol droht dem noch amtierenden amerikanischen Präsidenten Donald Trump – erstmals in der Geschichte der USA – ein zweites Amtsenthebungsverfahren.',
+    '{}'::jsonb
+)
+,(
+    'd4f4cc2d-4711-4e56-b2d0-4857aee35b13', 'Sollten alle drei Prozessschritte (Untersuchung und Anklage, Verfahrenseinleitung durch Wahl im Repräsentantenhaus, Anhörung und Verurteilung im Senat) erfolgreich sein, wird er sofort seines Amtes enthoben und könnte (auch nach Ende seiner Regierungszeit) einige oder alle seine Privilegien (Pension bis zum Lebensende, Reisebudget etc.) verlieren.',
+    '{"token": [], "spans": [], "compound": []}'::jsonb
+)
+,(
+    'e6db3858-5df3-40cd-bcbb-2fbf5daeab2c', 'Auf Bundesebene braucht es für eine Anklage (Impeachment) eine einfache Mehrheit im Repräsentantenhaus und danach für eine Verurteilung (Amtsenthebung) eine Zweidrittelmehrheit im Senat.',
+    '{"token": [{"lemma": "Impeachment", "span": [45, 56]}]}'::jsonb
+)
+,(
+    '49c53004-455c-4d99-a326-6220bf69006e', 'Diese Gefahr ist für ihn schwieriger zu kalkulieren als das Risiko eines Impeachments durch das Repräsentantenhaus, auf das er durch politische und legalistische Schachzüge nach wie vor Einfluß nehmen kann.',
+    '{"token": [], "spans": [], "compound": []}'::jsonb
+)
+,(
+    'ff05db90-ea35-483c-b183-c360258d679a', 'Impeachment ist das formelle Amtsenthebungsverfahren gegen den Präsidenten durch den Kongreß.',
+    '{"token": [{"lemma": "Impeachment", "span": [0, 11]}], "spans": [], "compound": []}'::jsonb
+)
+,(
+    '27919871-64b2-43f6-b31e-f64c44439008', 'Der Rechtsausschuß des Kongresses leitet das Impeachment ein.',
+    '{"token": [], "spans": [], "compound": [{"lemma": "Impeachment", "spans": [[45, 56]]}]}'::jsonb
+)
+,(
+    'ee14aaae-49ff-48f9-a4eb-9956c2574fe5', 'Nach Artikel 14 der US-Verfassung kann gegen den Präsidenten das sog. Impeachment (dt. Amtsenthebungs-Verfahren) eingeleitet werden, wenn ein schweres Verbrechen oder ein kriminelles Fehlverhalten (z. B. Meineid) vorliegt.',
+    '{"token": [{"lemma": "Impeachment", "span": [70, 81]}], "spans": [], "compound": []}'::jsonb
+)
+,(
+    '4c421bd7-d5ad-4749-a1ac-882b42df59ec', 'Die erste parlamentarische Abstimmung über das Impeachment wird von der Tagesordnung der Dumasitzung am kommenden Donnerstag gestrichen.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [47, 58]}], "compound": []}'::jsonb
+)
+,(
+    '79dc056b-9d7c-4c38-87dd-c888b75510aa', 'Aber er ließ keinen Zweifel daran, dass Rücktritt oder gar das Impeachment eine zu schwere Buße wären.',
+    '{"token": [], "spans": [], "compound": [{"lemma": "Impeachment", "spans": [[63, 74]]}]}'::jsonb
+)
+,(
+    'f2b98700-7e1f-40b5-b53e-532588651576', 'Nixons politische Stellung und die Frage, ob ein Impeachment (Absetzungsverfahren im Kongreß) eingeleitet werden soll, werden unterschiedlich bewertet.',
+    '{"token": [{"lemma": "Impeachment", "span": [49, 60]}], "spans": [], "compound": []}'::jsonb
+)
+,(
+    '1dc4b16b-d9f7-4e9c-aed8-adb4d0fb86e2', 'Statt dessen sollen Zeugen vor dem Ausschuß erklären, daß die Lewinsky-Affäre weit weniger schwer wiege als der Machtmißbrauch, der Präsident Nixon 1974 noch vor der Einleitung des Impeachments zum Rücktritt zwang.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [181, 193]}], "compound": []}'::jsonb
+)
+,(
+    '11b8c10f-3340-414a-9737-a1979c6eb1aa', 'Freilich erlangte Ford diese überwältigende Zustimmung nicht zuletztdeshalb, weil die meisten Demokraten davon ausgingen, er werde 1976 der denkbar schwächste republikanische Kandidat für das Amt des Präsidenten sein; der Rücktritt oder das Impeachment Richard Nixons waren damals noch nicht sicher voraussagbar.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [241, 252]}], "compound": []}'::jsonb
+)
+,(
+    'b85c4f88-809d-4bd7-accc-3aceb4ecda42', 'Es ist anzunehmen, daß Nixon im Falle eines „Impeachments“ freiwillig abdankte.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [45, 57]}], "compound": []}'::jsonb
+)
+,(
+    '7c578e06-420c-4577-bba3-7084134f593a', 'Nixon, so sagt Jenner, „sollte für die Handlungen seiner Mitarbeiter verantwortlich gemacht werden, selbst wenn er nicht wußte, daß einer von ihnen etwas tat, was zu einem Impeachment führen könnte, wenn der Präsident selbst es täte.“',
+    '{"token": [], "spans": [], "compound": [{"lemma": "Impeachment", "spans": [[172, 183]]}]}'::jsonb
+)
+,(
+    '13f37de4-6823-455f-b267-bd9d29156f28', 'Ob der zunächst geheim gebliebene Befund der grand jury über Nixons Rolle in der Affäre dem Repräsentantenhaus den entscheidenden Anstoß oder gar die Handhabe zum impeachment geben wird, läßt sich mit Sicherheit nicht voraussagen.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [163, 174]}], "compound": []}'::jsonb
+)
+,(
+    'ff9d91e9-df96-42de-afc3-7f0165c8a62f', 'Der Oberste Gerichtshof entschied am Mittwoch voriger Woche einstimmig, Nixon müsse die 64 Tonbänder herausgeben, die Sonderankläger Jaworski für den Prozeß gegen mehrere Präsidenten-Berater verlangt hat; der Rechtsausschuß des Repräsentantenhauses sprach sich in der Nacht zum Sonntag mit 27:11 Stimmen für ein Amtsenthebungsverfahren (Impeachment) aus.',
+    '{"token": [{"lemma": "Impeachment", "span": [337, 348]}], "spans": [], "compound": []}'::jsonb
+)
+,(
+    '1bd50eaa-7591-4a41-adf5-eac8a3af1dbf', 'Sollte das russische Unterhaus in der für den 15. April angesetzten Abstimmung über die Amtsenthebung mehrheitlich für das Impeachment stimmen, werde Präsident Boris Jelzin zur Auflösung des Parlaments schreiten, glaubt das Blatt.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [123, 134]}], "compound": []}'::jsonb
+)
+,(
+    'ca7c284c-78c7-478c-9671-a94b83241659', 
+    'Warum hat Boris Jelzin ausgerechnet unmittelbar vor der Abstimmung über das Impeachment den im Parlament angesehenen Primakow entlassen?',
+    '{"compound": [{"lemma": "Impeachment", "spans": [[76, 87]]}]}'::jsonb
+)
+,(
+    '17dcc929-2a4d-4228-a75e-62eaeb89ac9b', 
+    'Jelzin darf zwar das Parlament nicht auflösen, wenn ein Impeachment gegen ihn läuft.',
+    '{"token": [{"lemma": "Impeachment", "span": [56, 67]}], "spans": [], "compound": []}'::jsonb
+)
+,(
+    'f321b04c-412e-4e81-97db-64cf4f995685', 
+    'Sergej Stepaschin konnte fast zwei Drittel der Abgeordneten für sich gewinnen, mehr, als am Sonntag für das gescheiterte Impeachment gegen Jelzin stimmten.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [121, 132]}], "compound": []}'::jsonb
+)
+,(
+    'e984d00f-53c4-406a-9b4f-2ad2c1c90f8d', 
+    'Beobachter rechneten damit, daß die Duma im angeheizten Machtkampf mit Jelzin die notwendige Mehrheit für die Einleitung des Impeachments sammeln könnte.',
+    '{"token": [], "spans": [{"lemma": "Impeachment", "span": [125, 137]}], "compound": []}'::jsonb
+)
+ON CONFLICT DO NOTHING
+;
+
+
+-- 
+-- Generate fake example items
+-- A1. Whitespace Tokenization with STRING_TO_ARRAY
+-- A2. Expand sql array row to N txt rows (UNNEST)
+-- A3. Trim each string
+-- A4. Remove all non-alphanumeric symbols
+-- B. Add some non-saying context data
+-- C. Generate random score
+-- 
+INSERT INTO evidence.example_items (sentence_id, lemma, context, score)
+    SELECT sentence_id
+        , regexp_replace(regexp_replace(
+                UNNEST(STRING_TO_ARRAY(sentence_text, ' ')),
+                '[\s+]', '', 'g'), '\W+', '', 'g')::text as "lemma"
+        , '{"publisher": "DWDS Wörterbuch"}'::jsonb as "context"
+        , random()::double precision as "score"
+    FROM evidence.sentences_cache
+ON CONFLICT DO NOTHING
+;
+
