38ffcbe2571b41a9ff101908a702dcf9cde84edc
UH <554c46@gmail.com>
2021-06-14 23:50:20 +0200

    folders reorganized

---
+++ dbuser/setup/000-install.sql
@@ -0,0 +1,18 @@
+-- 
+-- Run the install.sql script to setup the database extensions, schema, tables, etc.
+-- the database is assumed to be named 'postgres'
+-- 
+
+-- 
+-- Load the extensions for the database
+-- 
+-- CREATE EXTENSION IF NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- CREATE EXTENSION btree_gist;
+CREATE EXTENSION pg_trgm;
+CREATE EXTENSION pgcrypto;
+
+-- 
+-- Create schemes
+-- 
+CREATE SCHEMA IF NOT EXISTS auth;
